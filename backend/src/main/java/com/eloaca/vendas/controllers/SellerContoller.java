40ee4c8888e174c0912014918e2d6a27ed37fd25
Eloa Cardozo Augusto <elooa_@hotmail.com>
2021-09-11 06:51:35 -0300

    Injetar @Autowired nos controllers

--- backend/src/main/java/com/eloaca/vendas/controllers/SellerContoller.java
+++ backend/src/main/java/com/eloaca/vendas/controllers/SellerContoller.java
@@ -2,7 +2,7 @@ package com.eloaca.vendas.controllers;
 
 import com.eloaca.vendas.dto.SellerDTO;
 import com.eloaca.vendas.services.SellerService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +12,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sellers")
-@AllArgsConstructor
 public class SellerContoller {
 
+    @Autowired
     private SellerService service;
 
     @GetMapping("/findAll")
