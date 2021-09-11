c94c363a4e48e482813f62f36714f7437837a762
Eloa Cardozo Augusto <elooa_@hotmail.com>
2021-09-11 07:39:26 -0300

    Adicionar anotacao @Table

--- backend/src/main/java/com/eloaca/vendas/entities/Seller.java
+++ backend/src/main/java/com/eloaca/vendas/entities/Seller.java
@@ -2,18 +2,18 @@ package com.eloaca.vendas.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
-@Entity(name = "tb_sellers")
+@Entity
+@Table(name = "tb_sellers")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
