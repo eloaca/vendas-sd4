d29639d3f72ceacb8371e37e6c451bd7238ab3f0
Eloa Cardozo Augusto <elooa_@hotmail.com>
2021-09-12 16:45:21 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -20,7 +20,7 @@ const DonutChart = () => {
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => x.sum);
 
-                setChartData = ({ labels: myLabels, series: mySeries });
+                setChartData({ labels: myLabels, series: mySeries });
             });
     }, []);
 
